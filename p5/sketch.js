9788b6676e567dd23d59743178a74c6e29eb0eda
littleflexer <43014213+littleflexer@users.noreply.github.com>
2019-01-10 14:16:40 -0600

    fixed

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,51 +1,27 @@
-// Major Project
+// Project Title
 // Your Name
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let name = "userinput";  //userinput
 let choice1;
 let choice2;
-let phraseInput, analyseButton, analyseButton2;
-let result;
-let canvas;
-
 
 function preload(){
+  choice1 = loadImage("assets/Choice 1.png");
+  choice2 = loadImage("assets/Choice 2.png");
 }
 
 
 function setup(){
-  let canvas = createCanvas(600, 600);
-  canvas.position(600, 100);
-
-  phraseInput = createInput("");
-  phraseInput.attribute("placeholder", "Enter a name!");
-  // phraseInput.parent("phrase");
-
-  analyseButton = createButton(choice1);
-  analyseButton.attribute("type", "button");  // note to self: button type instead of submit type
-  // analyseButton.parent("phrase");
-
-  analyseButton2 = createButton(choice2);
-  analyseButton2.attribute("type", "button");
-  // analyseButton2.parent("phrase");
-
-  result = createP();
-  // result.parent("container");
+  createCanvas(windowWidth, windowHeight);
+  background(200);
+  imageMode(CENTER);
+  image(choice1, windowWidth/4, windowHeight* (3/4), choice1.width, choice1.height);
+  image(choice2, windowWidth*(3/4), windowHeight* (3/4), choice2.width, choice2.height);
 }
 
 function draw() {
-  background(26, 78, 90);
-  imageMode(CENTER);
-  let choice1 = createImg("assets/Choice 1.png");
-  let choice2 = createImg("assets/Choice 2.png");
-  let body = createImg("assets/body.png");
 
-  body.size(100, 100);
-  choice1.position(350, 350);
-  choice2.position(650, 300);
-  body.position(825, 697);
 }
